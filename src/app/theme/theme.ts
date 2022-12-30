9cc943b00f54b8969509ad12ff5ce19ef7bfdffe
yambal <jy@yambal.net>
2022-12-30 16:20:26 +0900

    styled-component & xstyled

---
+++ src/app/theme/theme.ts
@@ -0,0 +1,10 @@
+import {
+  defaultTheme,
+  ThemeProvider,
+  Preflight,
+} from '@xstyled/styled-components'
+
+export const theme = {
+  ...defaultTheme,
+  // Customize your theme here
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,21 @@ import App from './App'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store/store'
+import {
+  ThemeProvider,
+  Preflight
+} from '@xstyled/styled-components'
+
+import { theme } from './app/theme/theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ThemeProvider theme={theme}>
+          <Preflight />
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
